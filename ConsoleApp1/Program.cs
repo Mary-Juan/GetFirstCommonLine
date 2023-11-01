10c469a6f4535ec5624afb8dea33165a041add76
mobocomputer <mobocomputer@TEST-SYSTEM>
2023-11-01 10:52:49 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("Enter first path:");
+string firstPath = Console.ReadLine();
+
+Console.WriteLine("Enter second path:");
+string secondPath = Console.ReadLine();
+
+
+
+void GetCommonLine(string path1, string path2)
+{
+    string[] fileA = File.ReadAllLines(path1);
+    string[] fileB = File.ReadAllLines(path2);
+
+    foreach(string lineA in fileA)
+    {
+        foreach(string lineB in fileB)
+        {
+            if(lineA == lineB)
+            {
+                Console.WriteLine(lineA);
+                System.Environment.Exit(0);
+            }
+        }
+    }
+}
